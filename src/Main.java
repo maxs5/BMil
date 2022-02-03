70f6758e8d0eb75cbbd45538fc6858a799258618
maxs5 <93215847+maxs5@users.noreply.github.com>
2022-02-03 09:15:50 +0300

    init

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        int priceTicket = 1005;
-        int priceMiles = 20;
+        int priceTicket = 1135;
+        int priceMiles = 30;
         int bonusMiles;
         bonusMiles = priceTicket / priceMiles;
         System.out.println("количество бонусных миль="+bonusMiles);
