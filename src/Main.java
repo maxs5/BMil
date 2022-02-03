a40ce12bb1956202344668bfc1ef274dd8ab91a0
maxs5 <93215847+maxs5@users.noreply.github.com>
2022-02-03 09:12:38 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int priceTicket = 1005;
+        int priceMiles = 20;
+        int bonusMiles;
+        bonusMiles = priceTicket / priceMiles;
+        System.out.println("количество бонусных миль="+bonusMiles);
+
+    }
+}
